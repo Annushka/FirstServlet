fc24e6a5fd5d207db81f47b3edce8877cc855d81
Annushka <reznic11@rambler.ru>
2013-10-04 17:24:32 +0400

    all comments in English!!!!!

--- Servlet.java
+++ Servlet.java
@@ -2,6 +2,12 @@
 //нажимаем на кнопочку и после нажатия то, что было написано в форме отражается 
 //в консоли нашего сервлета.
 
+
+// We already have a web page with form and button. 
+//Write somthing to the form, push the button and after pushing that text, 
+//that we have written to form is shown on eclipse console.
+
+
 //Servlet.java 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -27,11 +33,12 @@ public class Servlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		Map<String,String[]> map = request.getParameterMap();// ключ - название (в моем случае это text) значение - то.что было введено в поле
-		System.out.println(map.get("text")[0] ); //передача на консоль того, что ввели в форму
+		Map<String,String[]> map = request.getParameterMap();// key - is the title (in my case it is "text" ) and the value is that we have written in the form
+		System.out.println(map.get("text")[0] ); // transferm to the console that was written to form
 		
 		//request.setAttribute("is_ok", true);
-		
+		//We already have the class, that implements the connection with already created database.
+		//So, we write to database all text that was written to form on the dinamic page - wish.html
 		
 		//у нас уже есть класс TestDB, который реализует соединение с уже созданной базой данных.
 		// Так мы записываем в базу то, что было введено в форму на нашей динамической страничке wish.html
